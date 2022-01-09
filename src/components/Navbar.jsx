a5690e147a1157c9730a4ea106f4dc30f6c6bf74
Thairy <thairydaza@gmail.com>
2022-01-09 14:58:18 -0300

    Create of Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar(){
+    return (
+        <div className="navbar">
+            <button>Home</button>
+            <button>Cities</button>
+            <button>Users</button>
+        </div>
+    )
+}
+
+export default Navbar;
