6ee60f50f0d030c911076b9deeb892d8af8642aa
Thairy <thairydaza@gmail.com>
2022-01-12 16:43:46 -0300

    city card component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Navbar from "./Navbar";
+import CityCard from "./CityCard";
 
 function Home(){
     return (
-        <Navbar/>
+        <>
+     {/*    <Navbar/> */}
+        <CityCard/> 
+        </>
     )
 }
 
