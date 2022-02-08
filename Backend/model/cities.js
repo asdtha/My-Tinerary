52d0aba62a9ea0cd32579fa4f7d0e66676090783
Thairy <thairydaza@gmail.com>
2022-02-08 20:37:55 -0300

    cities model added

---
+++ Backend/model/cities.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+const citiesSchemas = new mongoose.Schema({
+
+    name: {type:String, required:true},
+    country:{type:String, required:true},
+    description:{type:String, required:true},
+    currency:{type:String, required:true},
+    language:{type:String, required:true},
+    demonym:{type:String, required:true},
+    population:{type:String, required:true},
+    coordinates:{type:String, required:true},
+    googlemaps:{type:String, required:true},
+    continent:{type:String, required:true},
+    region:{type:String, required:true},
+    flag:{type:String, required:true},
+    image:{type:String, required:true},
+
+}) 
+
+const Cities = mongoose.model("cities", cities.Schema)
+
+module.exports = Cities;
