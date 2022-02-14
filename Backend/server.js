8e1042387fe73615b62b9409599f409fad7affb3
Thairy <thairydaza@gmail.com>
2022-02-14 16:55:42 -0300

    server

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,16 @@
 require("dotenv").config()
 const express = require("express")
+const cors = require("cors")
 require("./config/database")
+
+const Router = require("./route/route")
+
 const app = express()
 //Usa los parametros de json que provengan de la base de datos
 app.use(express.json())
+app.use("/api", Router)
+app.use(cors())
+
 
 app.listen("4000", () => console.log("Servidor inicializado en puerto 4000"))
+
